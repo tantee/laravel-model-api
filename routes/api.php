5c2b9ff38dbe1e37675822f0ffa116d9db30bc35
Poompis Pattaranutaporn <poompis@yahoo.com>
2021-02-07 21:05:14 +0700

    add dependency to controller

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use TaNteE\LaravelModelApi\Http\Controllers\ModelAPIController;
 
 Route::group(["prefix"=>config('model-api.route-prefix'),"middleware"=>config('model-api.route-middleware')],function () {
   Route::prefix('models')->group(function () {
-    Route::get('{modelNamespace}/{modelName}','ModelAPIController@readRouting');
-    Route::post('{modelNamespace}/{modelName}/{method}','ModelAPIController@methodRouting');
+    Route::get('{modelNamespace}/{modelName}',[ModelAPIController::class,'readRouting']);
+    Route::post('{modelNamespace}/{modelName}/{method}',[ModelAPIController::class,'methodRouting']);
   });
 });
